838a721a5c24698f40980074d1693f54fa072838
Max Chernoff <git@maxchernoff.ca>
2025-03-07 05:27:02 -0700

    Add additional tests

--- conversion_test.go
+++ conversion_test.go
@@ -1,6 +1,8 @@
 package rfc2136
 
 import (
+	"encoding/base64"
+	"fmt"
 	"net"
 	"testing"
 	"time"
@@ -10,6 +12,9 @@ import (
 )
 
 const zone = "example.com."
+
+var echString = "AEj+DQBEAQAgACAdd+scUi0IYFsXnUIU7ko2Nd9+F8M26pAGZVpz/KrWPgAEAAEAAWQVZWNoLXNpdGVzLmV4YW1wbGUubmV0AAA="
+var echBytes, _ = base64.StdEncoding.DecodeString(echString)
 
 var testCases = map[dns.RR]libdns.Record{
 	&dns.TXT{
@@ -26,6 +31,7 @@ var testCases = map[dns.RR]libdns.Record{
 		Value: "\"hello world\"",
 		TTL:   220 * time.Second,
 	},
+
 	&dns.TXT{
 		Hdr: dns.RR_Header{
 			Name:   "txt.example.com.",
@@ -84,6 +90,93 @@ var testCases = map[dns.RR]libdns.Record{
 		Name:  "privateuse",
 		Value: `\# 5 0d10480001`,
 		TTL:   150 * time.Second,
+	},
+
+	&dns.HTTPS{
+		SVCB: dns.SVCB{
+			Hdr: dns.RR_Header{
+				Name:   "https.example.com.",
+				Rrtype: dns.TypeHTTPS,
+				Class:  dns.ClassINET,
+				Ttl:    150,
+			},
+			Priority: 2,
+			Target:   "target.example.com.",
+			Value: []dns.SVCBKeyValue{
+				&dns.SVCBAlpn{
+					Alpn: []string{"h3", "h2"},
+				},
+				&dns.SVCBIPv4Hint{
+					Hint: []net.IP{net.ParseIP("127.0.0.1")},
+				},
+				&dns.SVCBIPv6Hint{
+					Hint: []net.IP{net.ParseIP("::1")},
+				},
+				&dns.SVCBECHConfig{
+					ECH: echBytes,
+				},
+			},
+		},
+	}: {
+		Type:     "HTTPS",
+		Name:     "https",
+		TTL:      150 * time.Second,
+		Priority: 2,
+		Target:   "target.example.com.",
+		Value:    fmt.Sprintf(`alpn="h3,h2" ipv4hint="127.0.0.1" ipv6hint="::1" ech="%s"`, echString),
+	},
+
+	&dns.MX{
+		Hdr: dns.RR_Header{
+			Name:   "mx.example.com.",
+			Rrtype: dns.TypeMX,
+			Class:  dns.ClassINET,
+			Ttl:    150,
+		},
+		Preference: 10,
+		Mx:         "mail.example.com.",
+	}: {
+		Type:     "MX",
+		Name:     "mx",
+		Value:    "mail.example.com.",
+		TTL:      150 * time.Second,
+		Priority: 10,
+	},
+
+	&dns.SRV{
+		Hdr: dns.RR_Header{
+			Name:   "srv.example.com.",
+			Rrtype: dns.TypeSRV,
+			Class:  dns.ClassINET,
+			Ttl:    150,
+		},
+		Priority: 10,
+		Weight:   20,
+		Port:     443,
+		Target:   "service.example.com.",
+	}: {
+		Type:     "SRV",
+		Name:     "srv",
+		Value:    "443 service.example.com.",
+		TTL:      150 * time.Second,
+		Priority: 10,
+		Weight:   20,
+	},
+
+	// Bare name
+	&dns.A{
+		Hdr: dns.RR_Header{
+			Name:   "example.com.",
+			Rrtype: dns.TypeA,
+			Class:  dns.ClassINET,
+			Ttl:    300,
+		},
+		A: net.ParseIP("127.0.0.1"),
+	}: {
+		Type:  "A",
+		Name:  "",
+		Value: "127.0.0.1",
+		TTL:   300 * time.Second,
 	},
 }
 
